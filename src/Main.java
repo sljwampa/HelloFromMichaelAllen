88735bda0ddba1fccd48704f377c25f6bcf86371
joshnsmith <joshsmithsp@gmail.com>
2021-01-30 12:17:06 -0700

    Added my line to your file!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello, my name is Michael Allen");
+        System.out.println("Hello Michael, my name is Josh");
     }
 }
