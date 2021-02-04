d32cbc60993f6478d8a3548601366fa1c2bdab4d
Daniel Parker <parker1212@byui.edu>
2021-02-03 19:24:45 -0700

    Update Main.java
    Daniel edited the code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello, my name is Michael Allen");
         System.out.println("Hello Michael, my name is Josh");
+        System.out.print("Hello Michael, my name is Daniel");
     }
 }
